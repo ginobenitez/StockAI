2255a260fc3b3b511ce1607e49cf6464c464c0ff
Juan <115327434+JuaneX1@users.noreply.github.com>
2023-10-08 09:29:41 -0400

    Final AI

--- ai.py
+++ ai.py
@@ -1,115 +1,92 @@
 import yfinance as yf
 import pandas as pd
-from sklearn.metrics import precision_score
+from pandas.io.formats.style import Styler
 from sklearn.ensemble import RandomForestClassifier as RFC
+from sklearn.metrics import precision_score
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
 
-def predict(train, test, predictors, model):
-    model.fit(train[predictors], train["Target"])
-    preds = model.predict(test[predictors])
-    preds = pd.Series(preds, index=test.index, name="Predictions")
-    combined = pd.concat([test["Target"], preds], axis=1)
-    return combined
+def get_company_data(ticker):
+    try:
+        company = yf.Ticker(ticker)
+        company_data = company.history(period = "max")
+        return company_data
+    except Exception as e:
+        print("Error fetching data for company, please check again")
+        print(e)
+        return None
 
-def backtest(data, model, predictors, start=2500, step=250):
-    all_predictions = []
+def stock_price_prediction(ticker):
+    # Fetch historical stock data
+    currCompany = get_company_data(ticker)
 
-    for i in range(start, data.shape[0], step):
-        train = data.iloc[0:i].copy()
-        test = data.iloc[i:(i+step)].copy()
-        predictions = predict(train, test, predictors, model)
-        all_predictions.append(predictions)
-
-    return pd.concat(all_predictions)
-
-def predict(train, test, predictors, model):
-    model.fit(train[predictors], train["Target"])
-    preds = model.predict_proba(test[predictors])[:,1]# This line is different
-    preds[preds >= .6] = 1
-    preds[preds < .6] = 0
-    preds = pd.Series(preds, index=test.index, name="Predictions")
-    combined = pd.concat([test["Target"], preds], axis=1)
-    return combined
-
-def accuracy():
-    # Get data from the company based on the short
-    currCompany = yf.Ticker("^GSPC")
-    currCompany = currCompany.history(period = "max")
-
-    # All time historical data
-    currCompany.plot.line(y = "Close", use_index = True)
-
-    # Remove unnecessary data
-    del currCompany["Dividends"]
-    del currCompany["Stock Splits"]
-
-    # Create a new Column that will store the data for the predicted day
-    # and shift it to the last position
+    # Define features and target variable
+    predictor_list = ["Close", "Volume", "Open", "High", "Low"]
     currCompany["Tomorrow"] = currCompany["Close"].shift(-1)
-
-    # Set a target if tomorrow's price is greater than today
     currCompany["Target"] = (currCompany["Tomorrow"] > currCompany["Close"]).astype(int)
-
-    # Remove data before 1990
     currCompany = currCompany.loc["1995-01-01":].copy()
 
-    # increase estimator value later
-    model = RFC(n_estimators = 100, min_samples_split = 100, random_state = 1)
+    # Split the data into training and testing sets
+    training = currCompany.iloc[:-110]
+    testing = currCompany.iloc[-110:]
 
-    # Add first columns from data to the training stage,
-    # test with the last 100 columns
-    training = currCompany.iloc[:-100]
-    testing = currCompany.iloc[-100:]
+    # Hyperparameter tuning with GridSearchCV
+    '''param_grid = {
+        'n_estimators': [100, 200, 300],
+        'max_depth': [None, 10, 20],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4]
+    }'''
+    param_grid = {
+        'n_estimators': [100],# Change this to 100 later
+        'max_depth': [None, 10],
+        'min_samples_split': [2, 5],
+        'min_samples_leaf': [1, 2]
+    }
 
-    # These are the parameters the model with consider to make
-    # predictions
-    predictor_list = ["Close", "Volume", "Open", "High", "Low"]
-    model.fit(training[predictor_list], training["Target"])
+    model = RFC(random_state=1)
+    grid_search = GridSearchCV(model, param_grid, cv=5, scoring='precision')
+    grid_search.fit(training[predictor_list], training["Target"])
 
-    RFC(min_samples_split = 100, random_state = 1)
+    # Get the best model from the grid search
+    best_model = grid_search.best_estimator_
 
-    preds = model.predict(testing[predictor_list])
-    preds = pd.Series(preds, index = testing.index)
+    # Make predictions on the testing set
+    preds = best_model.predict(testing[predictor_list])
+    precision = precision_score(testing["Target"], preds)
 
-    precision_score(testing["Target"], preds)
+    # Map binary values to user-friendly labels using a list comprehension
+    label_mapping = {0: "Sell", 1: "Buy"}
+    actual_labels = [label_mapping[val] for val in testing["Target"]]
+    predicted_labels = [label_mapping[val] for val in preds]
 
-    combined = pd.concat([testing["Target"], preds], axis = 1)
+    # Create a DataFrame with predictions
+    predictions = pd.DataFrame({
+        "Date": testing.index.date,
+        "Actual": actual_labels,
+        "Predicted": predicted_labels
+    })
+    return predictions, precision
 
-    combined.plot()
+def plot_historical_data(ticker):
+    try:
+        company = yf.Ticker(ticker)
+        
+        # Fetch historical data for the last 15 years
+        end_date = pd.Timestamp.now()
+        start_date = end_date - pd.DateOffset(years=1)
+        company_data = company.history(start=start_date, end=end_date)
 
+        # Plot historical Close prices
+        plt.figure(figsize=(12, 6))
+        plt.plot(company_data.index, company_data['Close'], label=f'{ticker} Close Price', color='b')
+        plt.title(f'Historical Close Prices for {ticker} (Last 1 Year)')
+        plt.xlabel('Date')
+        plt.ylabel('Close Price')
+        plt.legend()
+        plt.grid()
+        plt.show()
 
-    predictions = backtest(currCompany, model, predictor_list)
-    predictions["Predictions"].value_counts()
-
-    precision_score(predictions["Target"], predictions["Predictions"])
-
-    predictions["Target"].value_counts() / predictions.shape[0]
-
-    horizons = [2, 5, 60, 250, 1000]
-
-    new_predictors = []
-    for horizon in horizons:
-        rolling_averages = currCompany.rolling(horizon).mean()
-
-        ratio_column = f"Close_Ratio_{horizon}"
-        currCompany[ratio_column] = currCompany["Close"] / rolling_averages["Close"]
-
-        trend_column = f"Trend_{horizon}"
-        currCompany[trend_column] = currCompany.shift(1).rolling(horizon).sum()["Target"]
-
-        new_predictors += [ratio_column, trend_column]
-
-        currCompany = currCompany.dropna()
-
-    model = RFC(n_estimators = 100, min_samples_split = 100, random_state = 1)
-
-    predictions = backtest(currCompany, model, new_predictors)
-
-    predictions["Predictions"].value_counts()
-
-    ps = precision_score(predictions["Target"], predictions["Predictions"])
-    
-    return ps    
-
-
-
-
+    except Exception as e:
+        print("Error fetching data for company, please check again")
+        
