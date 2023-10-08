0fa2c8edec9673af31a3843219c2bea0bb9685fc
Juan <115327434+JuaneX1@users.noreply.github.com>
2023-10-08 09:29:20 -0400

    This is the final GUI

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,  
-NavigationToolbar2Tk) 
-import tempai as ai
+import ai
 import yfinance as yf
+import pandas as pd
+from tkinter import scrolledtext
+
+pd.set_option('display.max_rows', None)
 
 def predict_stock():
     ticker = entry.get()
     if not ticker:
-        messagebox.showerroe("Error", "Please enter a ticker symbol")
+        messagebox.showerror("Error", "Please enter a ticker symbol")
         return
     
     try:
         predictions, precision = ai.stock_price_prediction(ticker)
-        result_label.config(text=f"Predictions for {ticker}:\n{predictions}\n\nEstimated Precision Score (Not Actual): {precision}")
-        figure  = ai.plot_historical_data(ticker)
-        
-        #canvas = FigureCanvasTkAgg(figure, graph_frame)
-        #canvas.draw()
-        #canvas.get_tk_widget.pack()
-        #figure.draw() 
-  
-        # placing the canvas on the Tkinter window 
-    
-        
+        result_text.delete(1.0, tk.END)
+        result_text.insert(tk.END, f"Predictions for {ticker}:\n{predictions}\n\nEstimated Precision Score (Not Actual): {precision}")
+        ai.plot_historical_data(ticker)
     except Exception as e:
         messagebox.showerror("Error", f"Error fetching data: {str(e)}")
 
-# Create the main window
 root = tk.Tk()
-root.title("Stock Price Prediction App")
-root.geometry("750x450")
+root.title("StockAI")
+root.geometry("600x400")
 
-# Create and pack widgets
+# Configure a light theme
+root.tk_setPalette(background="white", foreground="black")
+style = ttk.Style()
+style.configure("TButton", foreground="black", borderwidth=0, relief="flat", background="white")
+style.configure("TLabel", foreground="black", background="white")
+style.map("TButton", background=[("active", "lightblue")])
+
 frame = ttk.Frame(root)
-
 frame.pack(padx=20, pady=20)
-#graph_frame = ttk.Frame(root)
+
+title_label = ttk.Label(frame, text="StockAI", font=("Helvetica", 36))
+title_label.pack()
 
 label = ttk.Label(frame, text="Enter Company Ticker:")
 label.pack()
@@ -48,11 +48,7 @@ entry.pack()
 predict_button = ttk.Button(frame, text="Predict", command=predict_stock)
 predict_button.pack()
 
-result_label = ttk.Label(frame, text="")
+result_text = scrolledtext.ScrolledText(frame, wrap=tk.WORD, width=60, height=20)
+result_text.pack()
 
-result_label.pack()
-
-#graph_frame.pack()
-
-# Start the Tkinter main loop
 root.mainloop()
