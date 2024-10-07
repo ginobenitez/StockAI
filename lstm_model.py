869bc3e0303222bc8a74e4c6e191dabea66dc07a
Gino Benitez <benitezgp81@gmail.com>
2024-10-07 00:01:54 -0400

    added env and new model

---
+++ lstm_model.py
@@ -0,0 +1,60 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+from alpha_vantage.timeseries import TimeSeries 
+# importing os module for environment variables
+import os
+# importing necessary functions from dotenv library
+from dotenv import load_dotenv, dotenv_values 
+# loading variables from .env file
+
+import sys
+print(sys.executable)
+
+
+load_dotenv() 
+
+print("All libraries loaded")
+print(os.getenv("API_KEY"))
+config = {
+    "alpha_vantage": {
+        "key": os.getenv("API_KEY"), # you can use the demo API key for this project, but please make sure to get your own API key at https://www.alphavantage.co/support/#api-key
+        "symbol": "IBM",
+        "outputsize": "full",
+        "key_adjusted_close": "5. adjusted close",
+    },
+    "data": {
+        "window_size": 20,
+        "train_split_size": 0.80,
+    }, 
+    "plots": {
+        "xticks_interval": 90, # show a date every 90 days
+        "color_actual": "#001f3f",
+        "color_train": "#3D9970",
+        "color_val": "#0074D9",
+        "color_pred_train": "#3D9970",
+        "color_pred_val": "#0074D9",
+        "color_pred_test": "#FF4136",
+    },
+    "model": {
+        "input_size": 1, # since we are only using 1 feature, close price
+        "num_lstm_layers": 2,
+        "lstm_size": 32,
+        "dropout": 0.2,
+    },
+    "training": {
+        "device": "cpu", # "cuda" or "cpu"
+        "batch_size": 64,
+        "num_epoch": 100,
+        "learning_rate": 0.01,
+        "scheduler_step_size": 40,
+    }
+}
