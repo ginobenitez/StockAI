d0b0bfc9dff84c5c6f62e91aca20aa6555afa974
ginobenitez <benitezgp81@gmail.com>
2024-10-11 15:34:07 -0400

    Update ai.py

--- ai.py
+++ ai.py
@@ -7,29 +7,14 @@ from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
 
 def get_company_data(ticker):
-    """try:
+    try:
         company = yf.Ticker(ticker)
         company_data = company.history(period = "max")
         return company_data
     except Exception as e:
         print("Error fetching data for company, please check again")
         print(e)
-        return None"""
-    sp500 = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
-
-    sp500['Symbol'] = sp500['Symbol'].str.replace('.', '-')
-
-    symbols_list = sp500['Symbol'].unique().tolist()
-
-    end_date = '2023-09-27'
-
-    start_date = pd.to_datetime(end_date)-pd.DateOffset(365*8)
-
-    df = yf.download(tickers=symbols_list,
-                    start=start_date,
-                    end=end_date).stack()
-
-    df.index.names = ['date', 'ticker']
+        return None
 
 def stock_price_prediction(ticker):
     # Fetch historical stock data
